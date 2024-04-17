c8406ea8290c68fb21eb2a1468886033a4d613cf
NicFod99 <nicola.foddis@studenti.unipd.it>
2024-04-17 16:15:46 +0200

    Text Display in favorite

--- sustainable_moving/lib/Screens/favoritePage.dart
+++ sustainable_moving/lib/Screens/favoritePage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:sustainable_moving/Models/favorite.dart';
+import 'package:sustainable_moving/Screens/home.dart';
 
 class FavoritePage extends StatelessWidget {
   const FavoritePage({Key? key}) : super(key: key);
@@ -13,26 +14,55 @@ class FavoritePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         actions: [
-          //Here, we only need to set the action to perform after the button is pressed. So, it is not necessary to use a Consumer.
-          //This approach won't cause the IconButton widget to rebuild when the notifyListeners() method is called.
           IconButton(
-              onPressed: () =>
-                  Provider.of<Favorite>(context, listen: false).clearFavorite(),
-              icon: Icon(Icons.delete))
+            onPressed: () =>
+                Provider.of<Favorite>(context, listen: false).clearFavorite(),
+            icon: Icon(Icons.delete),
+          )
         ],
         centerTitle: true,
         title: Text("Favorite places"),
       ),
-      body: Center(
-        //On the other hand, here we need a Consumer, since we want the UI to update if the notifyListeners() method is called
-        // for example, after the tap of the IconButton in the AppBar.
-        child: Consumer<Favorite>(
-          builder: (context, favorite, child) {
-            return Text(
-                'You have ${favorite.favorites.length} favorite places.');
-          },
-        ),
+      body: Consumer<Favorite>(
+        builder: (context, favorite, child) {
+          return GridView.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              mainAxisSpacing: 4.0,
+              crossAxisSpacing: 4.0,
+              childAspectRatio: 0.75,
+            ),
+            itemCount: favorite.favorites.length,
+            itemBuilder: (BuildContext context, int index) {
+              // Get the corresponding PathData object from the imageText list
+              PathData pathData = imageText[index].name;
+              return Column(
+                children: [
+                  Image.asset(
+                    favorite.favorites[
+                        index], // Assuming favorites contain image paths
+                    fit: BoxFit.cover,
+                  ),
+                  Text(
+                    pathData.name, // Display the name from the PathData object
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    pathData
+                        .description, // Display the description from the PathData object
+                    style: TextStyle(
+                      fontSize: 14,
+                    ),
+                  ),
+                ],
+              );
+            },
+          );
+        },
       ),
     );
-  } //build
-} //CartPage
+  }
+}
