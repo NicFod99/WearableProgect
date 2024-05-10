6b910bd7f5544b018ecac917339e5cace46db42c
Alessandro Discalzi <alessandrodiscalzi98@gmail.com>
2024-05-10 09:58:55 +0200

    fixes

--- sustainable_moving/lib/Impact/impact.dart
+++ sustainable_moving/lib/Impact/impact.dart
@@ -29,17 +29,3 @@ class ImpactHR {
   static String patientUsername = 'Jpefaq6m58';
 }//Impact//Impact
 
-class ImpactDistance {
-  static String baseUrl = 'https://impact.dei.unipd.it/bwthw/';
-  static String pingEndpoint = 'gate/v1/ping/';
-  static String tokenEndpoint = 'gate/v1/token/';
-  static String refreshEndpoint = 'gate/v1/refresh/';
-
-  static String distanceEndpoint = 'data/v1/distance/patients/'; // CAMBIA QUI
-
-  static String username = 'qDBhiJ4dRo';
-  static String password = '12345678!';
-
-  static String patientUsername = 'Jpefaq6m58';
-}//Impact
-
--- sustainable_moving/lib/Models/distanceNotifier.dart
+++ sustainable_moving/lib/Models/distanceNotifier.dart
@@ -1,40 +1,44 @@
+// Importing necessary libraries
 import 'dart:convert';
 import 'dart:io';
-import 'package:jwt_decoder/jwt_decoder.dart';
-//import 'package:sustainable_moving/Models/featuresGraph.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sustainable_moving/Models/distance.dart';
 import 'package:sustainable_moving/Impact/impact.dart';
-import 'package:draw_graph/models/feature.dart';
+import 'package:logger/logger.dart';
 
-/* NOTIFIER DI DISTANCE, aggiungere qui le funzioni per ottimizzare il codice
- * (vedi Heart Rate Notifier) */
-
+// DistanceNotifier class extends ChangeNotifier to provide notification to its listeners when a change occurs
 class DistanceNotifier extends ChangeNotifier {
-  //For simplicity, a product is just a String.
+  // Initialize the logger
+  var logger = Logger();
+  // List to store distances
   List<Distance> distances = [];
-  // Method to add a product to the list
+
+  // Method to add a distance to the list
   void addProduct(Distance product) {
     distances.add(product);
-    notifyListeners();
-  } //addProduct
-
-  void clearFavorite() {
-    distances.clear();
-    //Call the notifyListeners() method to alert that something happened.
+    // Notify all the listeners about this update
     notifyListeners();
   }
 
-  Future<int?> authorize() async {
-    final url = Impact.baseUrl + Impact.tokenEndpoint;
+  // Method to clear all distances
+  void clearFavorite() {
+    distances.clear();
+    // Notify all the listeners about this update
+    notifyListeners();
+  }
 
+  // Method to authorize the user
+  Future<int?> authorize() async {
+    // Prepare the URL and body for the POST request
+    final url = Impact.baseUrl + Impact.tokenEndpoint;
     final body = {'username': Impact.username, 'password': Impact.password};
 
-    //print('Calling: $url');
+    // Send the POST request
     final response = await http.post(Uri.parse(url), body: body);
 
+    // If the response is successful, save the access and refresh tokens
     if (response.statusCode == 200) {
       final decodedResponse = jsonDecode(response.body);
       final sp = await SharedPreferences.getInstance();
@@ -42,48 +46,61 @@ class DistanceNotifier extends ChangeNotifier {
       sp.setString('refresh', decodedResponse['refresh']);
     }
 
+    // Return the status code of the response
     return response.statusCode;
   }
 
+  // Method to refresh the tokens
   Future<int> refreshTokens() async {
-    final url = ImpactHR.baseUrl + ImpactHR.refreshEndpoint;
+    // Prepare the URL and body for the POST request
+    final url = Impact.baseUrl + Impact.refreshEndpoint;
     final sp = await SharedPreferences.getInstance();
     final refresh = sp.getString('refresh');
     final body = {'refresh': refresh};
 
+    // Send the POST request
     final response = await http.post(Uri.parse(url), body: body);
 
+    // If the response is successful, update the access and refresh tokens
     if (response.statusCode == 200) {
       final decodedResponse = jsonDecode(response.body);
       sp.setString('access', decodedResponse['access']);
       sp.setString('refresh', decodedResponse['refresh']);
     }
 
+    // Return the status code of the response
     return response.statusCode;
   }
 
+  // Method to get the distance
   Future<void> getDistance() async {
+    // Request the data
     List<Distance>? distance = await _requestData();
+    // If the data is not null and not empty, update the distances
     if (distance != null && distance.isNotEmpty) {
       distances = distance;
     } else {
-      print("Unable to fetch Distance datas...");
+      // Log the error message
+      logger.e("Unable to fetch Distance datas...");
     }
+    // Notify all the listeners about this update
     notifyListeners();
   }
 
+  // Method to request the data
   Future<List<Distance>?> _requestData() async {
     List<Distance>? result;
     final sp = await SharedPreferences.getInstance();
     final access = sp.getString('access');
-    //final day = DateTime.now().day;
-    final day = '2023-05-04';
+    const day = '2023-05-04';
     final url = '${Impact.baseUrl}${Impact.distanceEndpoint}${Impact.patientUsername}/day/$day/';
-    ;
+
+    // Send the GET request
     final response = await http.get(Uri.parse(url), headers: {
       HttpHeaders.authorizationHeader: 'Bearer $access',
     });
 
+    // If the response is successful, parse the data
     if (response.statusCode == 200) {
       final decodedResponse = jsonDecode(response.body);
       result = [];
@@ -94,13 +111,14 @@ class DistanceNotifier extends ChangeNotifier {
       result = null;
     }
 
+    // Return the result
     return result;
   }
 
-  List<double> _dataPicker() {
-    List<double> distanceList =
-        distances.map((e) => e.value.toDouble()).toList();
+  // Method to pick the data
+  List<int> _dataPicker() {
+    // Map the distances to their integer values and take the first 5
+    List<int> distanceList = distances.map((e) => e.value.toInt()).toList();
     return distanceList.take(5).toList();
   } 
-
-}//Cart
+}
