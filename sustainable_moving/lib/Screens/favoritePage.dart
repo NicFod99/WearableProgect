975d727afb12629c46f7476c7240a0502be3c2e0
Alberto <albesavoldelli@gmail.com>
2024-06-11 15:18:54 +0200

    pop-up description on favoritePage

--- sustainable_moving/lib/Screens/favoritePage.dart
+++ sustainable_moving/lib/Screens/favoritePage.dart
@@ -11,6 +11,46 @@ class FavoritePage extends StatelessWidget {
 
   static const routename = 'Favoritepage';
 
+  void _showDescription(
+      BuildContext context, String name, String description, double length) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(
+            name,
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(description),
+              SizedBox(height: 10),
+              Row(
+                children: [
+                  Text(
+                    'Length: ',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Text('$length km'),
+                ],
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Close'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     print('${FavoritePage.routename} built');
@@ -32,48 +72,56 @@ class FavoritePage extends StatelessWidget {
         itemBuilder: (BuildContext context, int index) {
           PathData pathData = item.products[index];
 
-          return Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Stack(
-                children: [
-                  Image.asset(
-                    pathData.imagePath,
-                    fit: BoxFit.fitWidth,
-                  ),
-                  Positioned(
-                    left: 0,
-                    right: 0,
-                    bottom: 0,
-                    child: Container(
-                      color: Colors.black.withOpacity(0.5),
-                      padding: EdgeInsets.all(8),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            pathData.name,
-                            style: const TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
+          return GestureDetector(
+            onTap: () => _showDescription(
+              context,
+              pathData.name,
+              pathData.description,
+              pathData.length,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Stack(
+                  children: [
+                    Image.asset(
+                      pathData.imagePath,
+                      fit: BoxFit.fitWidth,
+                    ),
+                    Positioned(
+                      left: 0,
+                      right: 0,
+                      bottom: 0,
+                      child: Container(
+                        color: Colors.black.withOpacity(0.5),
+                        padding: EdgeInsets.all(8),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              pathData.name,
+                              style: const TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
                             ),
-                          ),
-                          Text(
-                            "Length: ${pathData.length} m",
-                            style: const TextStyle(
-                              fontSize: 14,
-                              color: Colors.white,
+                            Text(
+                              "Length: ${pathData.length} m",
+                              style: const TextStyle(
+                                fontSize: 14,
+                                color: Colors.white,
+                              ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 16),
-            ],
+                  ],
+                ),
+                SizedBox(height: 16),
+              ],
+            ),
           );
         },
       ),
