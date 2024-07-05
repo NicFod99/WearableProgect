dc6b55210340528bf4db351eee7286edb79ea265
Alberto <albesavoldelli@gmail.com>
2024-07-05 15:31:02 +0200

    fix labels

--- sustainable_moving/lib/Models/items.dart
+++ sustainable_moving/lib/Models/items.dart
@@ -25,13 +25,14 @@ class Catalog {
       description:
           'From the Bassanello barrier to the Via D’Acquapendente bridge.',
       imagePath: 'assets/Pd1.jpg',
-      mapUrl: "https://www.google.com/maps/dir/Bassanello,+Padova,+PD/Ponte+di+San+Lorenzo,+Via+S.+Francesco,+28,+35123+Padova+PD/@45.396751,11.862346,15z/data=!3m1!4b1!4m14!4m13!1m5!1m1!1s0x477edba4dc863141:0x16c8f8fd6a38387f!2m2!1d11.8675778!2d45.3870423!1m5!1m1!1s0x477eda50f76a1867:0x386b5feaf039e053!2m2!1d11.8776769!2d45.4062566!3e2?entry=ttu",
+      mapUrl:
+          "https://www.google.com/maps/dir/Bassanello,+Padova,+PD/Ponte+di+San+Lorenzo,+Via+S.+Francesco,+28,+35123+Padova+PD/@45.396751,11.862346,15z/data=!3m1!4b1!4m14!4m13!1m5!1m1!1s0x477edba4dc863141:0x16c8f8fd6a38387f!2m2!1d11.8675778!2d45.3870423!1m5!1m1!1s0x477eda50f76a1867:0x386b5feaf039e053!2m2!1d11.8776769!2d45.4062566!3e2?entry=ttu",
     ),
     PathData(
       name: 'Città Giardino path',
       length: 2300,
       description:
-          'From the Bassanello barrier cross the intersection keeping to the right of the bridge. As soon as you make the curve to the right, get off the sidewalk and run next to the river on the small grass/beaten earth road. Pass under the footbridge and walk along the road until you reach the Saracensca Bridge. Cross the bridge and turn immediately right to walk along the high tree-lined embankment of Garden City until you reach the iron footbridge. Walk along it and turn left. Get off the sidewalk and retrace the route taken on the way out.',
+          'From the Bassanello barrier cross the intersection keeping to the right of the bridge. As soon as you make the curve to the right, get off the sidewalk and run next to the river on the small grass/beaten earth road. Pass under the footbridge and walk along the road until you reach the Saracinesca Bridge. Cross the bridge and turn immediately right to walk along the high tree-lined embankment of Città Giardino until you reach the iron footbridge. Walk along it and turn left. Get off the sidewalk and retrace the route taken on the way out.',
       imagePath: 'assets/Pd2.jpg',
       mapUrl: "",
     ),
@@ -41,7 +42,8 @@ class Catalog {
       description:
           'From the Bassanello barrier to the Via Facciolati bridge. Stop before the second bridge at the intersection with the road and return. At the bridge with Acquapendente Street go under the bridge with small downhill road you find on the right before the bridge.',
       imagePath: 'assets/Pd3.jpg',
-      mapUrl: "https://www.google.com/maps/dir/Bassanello,+Padova,+PD/45.3836086,11.8993081/@45.3853274,11.8732328,15z/data=!3m1!4b1!4m9!4m8!1m5!1m1!1s0x477edba4dc863141:0x16c8f8fd6a38387f!2m2!1d11.8675778!2d45.3870423!1m0!3e2?entry=ttu",
+      mapUrl:
+          "https://www.google.com/maps/dir/Bassanello,+Padova,+PD/45.3836086,11.8993081/@45.3853274,11.8732328,15z/data=!3m1!4b1!4m9!4m8!1m5!1m1!1s0x477edba4dc863141:0x16c8f8fd6a38387f!2m2!1d11.8675778!2d45.3870423!1m0!3e2?entry=ttu",
     ),
     PathData(
       name: 'Terranegra path',
